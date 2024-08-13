f46db2231dabae7d7db01c30ee6b9b1c5b1d2ada
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2024-08-13 15:06:37 -0400

    Sign In Validation Done

--- Script/signin.js
+++ Script/signin.js
@@ -9,6 +9,38 @@ let passwordInput = document.getElementById("password-input");
 let signInButton = document.getElementById("sign-in");
 let buttonContainer = document.getElementById("button-container");
 let spinnerContainer = document.getElementById("spinner-container");
+let emailError = document.getElementById("email-error");
+let passwordError = document.getElementById("password-error");
+let isEmailValid;
+let ispasswordValid;
+
+emailInput.addEventListener("input", function () {
+  if (emailInput.value === "") {
+    emailError.style.display = "block";
+    emailInput.style.borderColor = "red";
+    emailInput.style.borderWidth = "2px";
+    isEmailValid = false;
+  } else {
+    emailError.style.display = "none";
+    emailInput.style.borderColor = "gray";
+    emailInput.style.borderWidth = "1px";
+    isEmailValid = true;
+  }
+});
+
+passwordInput.addEventListener("input", function () {
+  if (passwordInput.value === "") {
+    passwordError.style.display = "block";
+    passwordInput.style.borderColor = "red";
+    passwordInput.style.borderWidth = "2px";
+    ispasswordValid = false;
+  } else {
+    passwordError.style.display = "none";
+    passwordInput.style.borderColor = "gray";
+    passwordInput.style.borderWidth = "1px";
+    ispasswordValid = true;
+  }
+});
 
 function signIn() {
   const auth = getAuth();
@@ -28,4 +60,8 @@ function signIn() {
     });
 }
 
-signInButton.addEventListener("click", signIn);
+signInButton.addEventListener("click", function () {
+  if (isEmailValid && ispasswordValid) {
+    signIn();
+  }
+});
